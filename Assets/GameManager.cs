d61c3ea3dbb528cb0394853ca80f5bad6a7d4238
NatioFla <golden.btb.takin@gmail.com>
2023-09-17 20:32:52 +0900

    jsonからピースを生成

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 using static System.Console;
 using jsontype;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -19,11 +21,78 @@ public class GameManager : MonoBehaviour
     int[,,] power_line;
     int[,] distance;
     bool[,] board;
+   
+    public GameObject block;
+    public GameObject piece;
+    public GameObject wire;
+    public List<GameObject> blockList;
+    public List<GameObject> pieceList;
+    public List<GameObject> wireList;
+    public RawImage image;
 
     void initialize(data obj)
     {
+        string imagePath = new string("Null");
+        
+        for(int i = 1;i<=obj.pieces.Count;i++){
 
+            pieceList.Add((GameObject)Instantiate(piece, new Vector3((i-1)*5,0,0), Quaternion.identity));
+            pieceList[i-1].name = "Mino" + i.ToString();
+        
+            for(int j = 1;j<=obj.pieces[i-1].cells.Count;j++){
+                
+                Vector3 pos = new Vector3(obj.pieces[i-1].cells[j-1].x,obj.pieces[i-1].cells[j-1].y,0.0f);
+                blockList.Add((GameObject)Instantiate(block, pos, Quaternion.identity));
+                blockList[j-1].transform.parent = pieceList[i-1].transform;
+
+                blockList[j-1].name = "Block" + j.ToString();
+
+                imagePath = obj.pieces[i-1].cells[j-1].texture.ToString();
+                imagePath = imagePath.Replace(".png","");
+                //Debug.Log(imagePath);
+                
+                String target = "Block"+j.ToString()+"/Canvas/RawImage";
+                RawImage image = GameObject.Find(target).GetComponent<RawImage>();
+
+                image.texture = Resources.Load<Texture2D>(imagePath);
+
+                for(int k = 1;k<=obj.pieces[i-1].cells[j-1].wireInterface.Count;k++){
+                    if(obj.pieces[i-1].cells[j-1].wireInterface[k-1]){
+                        Debug.Log(k);
+                        switch(k){
+                            case 1:
+                                //GameObject thumb = (GameObject)Instantiate(wire, pos , Quaternion.Euler(0f,0f,0f));
+                                //wireList.Add(thumb);
+                                //thumb.transform.parent = blockList[i-1].transform;
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0f, 0.25f, 0f), Quaternion.Euler(0f,0f,0f)));
+                                break;
+                            case 2:
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0.25f, 0f, 0f), Quaternion.Euler(0f,0f,90f)));
+                                break;
+                            case 3:
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0f, -0.25f, 0f), Quaternion.Euler(0f,0f,180f)));
+                                break;
+                            case 4:
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(-0.25f, 0.25f, 0f), Quaternion.Euler(0f,0f,270f)));
+                                break;
+                            default:
+                                break;
+                        
+                        }
+
+                        //wireList[k-1].transform.parent = pieceList[i-1].transform;
+                    }
+                }
+                
+            }
+
+            
+
+        }
+        //Debug.Log("ok");
+                
     }
+    
 
     void OnUpdate()
     {
