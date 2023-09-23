79dbaf03d1cb604ae7dd0f30b2eb7e99cbcde628
mono-1729 <kawai.172957@gmail.com>
2023-09-23 19:11:56 +0900

    爆発エフェクトの追加

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
     int[,,] power_line;
     int[,] distance;
     public bool[,] board;
-   
+
     public GameObject block;
     public GameObject piece;
     public GameObject wire;
@@ -32,21 +32,22 @@ public class GameManager : MonoBehaviour
     public GameObject lightning;
     public GameObject lightningParent;
     public GameObject tileParent;
+    public GameObject explosion;
     public List<GameObject> pieceList;
     public List<Piece> pieceScriptList;
     public List<GameObject> wireList;
     public List<GameObject> elecList;
     public List<Piece> elecScriptList;
     public RawImage image;
-    public List<Tuple<int,int>> elecposList = new List<Tuple<int, int>>();
+    public List<Tuple<int, int>> elecposList = new List<Tuple<int, int>>();
 
     void initializePiece(data obj)
     {
         string imagePath = new string("Null");
         int block_cnt = 0;
-        
-        for(int i = 0;i<obj.pieces.Count;i++){
-            float init_x = -3+i*2;
+
+        for (int i = 0; i < obj.pieces.Count; i++) {
+            float init_x = -3 + i * 2;
             float init_y = -3;
             float init_z = -(0.1f * i);
             if (obj.pieces[i].type == "electronics")
@@ -57,7 +58,7 @@ public class GameManager : MonoBehaviour
                 elecposList.Add(Tuple.Create(obj.pieces[i].cells[0].x, obj.pieces[i].cells[0].y));
             }
 
-            pieceList.Add((GameObject)Instantiate(piece, new Vector3(init_x,init_y,init_z), Quaternion.identity));
+            pieceList.Add((GameObject)Instantiate(piece, new Vector3(init_x, init_y, init_z), Quaternion.identity));
             pieceList[i].name = "Mino" + i.ToString();
             pieceList[i].AddComponent<Piece>();
             pieceScriptList.Add(pieceList[i].GetComponent<Piece>());
@@ -71,9 +72,9 @@ public class GameManager : MonoBehaviour
             }
 
 
-            for (int j = 0; j < obj.pieces[i].cells.Count;j++){
-                
-                Vector3 pos = new Vector3(init_x+obj.pieces[i].cells[j].x,init_y+obj.pieces[i].cells[j].y,0);
+            for (int j = 0; j < obj.pieces[i].cells.Count; j++) {
+
+                Vector3 pos = new Vector3(init_x + obj.pieces[i].cells[j].x, init_y + obj.pieces[i].cells[j].y, 0);
                 pieceScriptList[i].cells.Add((GameObject)Instantiate(block, pos, Quaternion.identity));
                 pieceScriptList[i].cells.Last().transform.parent = pieceList[i].transform;
                 pieceScriptList[i].cells.Last().name = "Block" + (block_cnt + j).ToString();
@@ -83,35 +84,35 @@ public class GameManager : MonoBehaviour
                 imagePath = obj.pieces[i].cells[j].texture.ToString();
                 //imagePath = imagePath.Replace(".png","");
                 //Debug.Log(imagePath);
-                
-                String target = "Block"+(block_cnt+j).ToString()+"/Canvas/RawImage";
+
+                String target = "Block" + (block_cnt + j).ToString() + "/Canvas/RawImage";
                 RawImage image = GameObject.Find(target).GetComponent<RawImage>();
 
                 image.texture = Resources.Load<Texture2D>(imagePath);
 
-                for(int k = 0;k<obj.pieces[i].cells[j].wireInterface.Count;k++){
+                for (int k = 0; k < obj.pieces[i].cells[j].wireInterface.Count; k++) {
                     pieceCellScript.wireInterfase.Add(obj.pieces[i].cells[j].wireInterface[k]);
                     if (obj.pieces[i].cells[j].wireInterface[k] && obj.pieces[i].type == "mino")
                     {
-                        switch(k){
+                        switch (k) {
                             case 0:
                                 //GameObject thumb = (GameObject)Instantiate(wire, pos , Quaternion.Euler(0f,0f,0f));
                                 //wireList.Add(thumb);
                                 //thumb.transform.parent = blockList[i-1].transform;
-                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0f, 0.25f, -0.05f), Quaternion.Euler(0f,0f,0f)));
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0f, 0.25f, -0.05f), Quaternion.Euler(0f, 0f, 0f)));
                                 break;
                             case 1:
-                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0.25f, 0f, -0.05f), Quaternion.Euler(0f,0f,90f)));
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0.25f, 0f, -0.05f), Quaternion.Euler(0f, 0f, 90f)));
                                 break;
                             case 2:
-                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0f, -0.25f, -0.05f), Quaternion.Euler(0f,0f,180f)));
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(0f, -0.25f, -0.05f), Quaternion.Euler(0f, 0f, 180f)));
                                 break;
                             case 3:
-                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(-0.25f,0, -0.05f), Quaternion.Euler(0f,0f,270f)));
+                                wireList.Add((GameObject)Instantiate(wire, pos + new Vector3(-0.25f, 0, -0.05f), Quaternion.Euler(0f, 0f, 270f)));
                                 break;
                             default:
                                 break;
-                        
+
                         }
 
                         wireList.Last().transform.parent = pieceScriptList[i].cells.Last().transform;
@@ -119,7 +120,7 @@ public class GameManager : MonoBehaviour
                 }
             }
             block_cnt += obj.pieces[i].cells.Count;
-            if(obj.pieces[i].type == "mino")
+            if (obj.pieces[i].type == "mino")
             {
                 pieceScriptList[i].MoveToInitialPosition();
             }
@@ -145,14 +146,14 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        GameObject start = (GameObject)Instantiate(tile, new Vector3(sx/2+0.5f, sy/2+0.5f, 1f), Quaternion.identity);
+        GameObject start = (GameObject)Instantiate(tile, new Vector3(sx / 2 + 0.5f, sy / 2 + 0.5f, 1f), Quaternion.identity);
         start.transform.parent = tileParent.transform;
         start.name = "start";
         String target = "start" + "/Canvas/RawImage";
         RawImage image = GameObject.Find(target).GetComponent<RawImage>();
         image.texture = Resources.Load<Texture2D>("start");
 
-        GameObject goal = (GameObject)Instantiate(tile, new Vector3(gx/2+0.5f, gy/2+0.5f, 1f), Quaternion.identity);
+        GameObject goal = (GameObject)Instantiate(tile, new Vector3(gx / 2 + 0.5f, gy / 2 + 0.5f, 1f), Quaternion.identity);
         goal.transform.parent = tileParent.transform;
         goal.name = "goal";
         target = "goal" + "/Canvas/RawImage";
@@ -164,9 +165,9 @@ public class GameManager : MonoBehaviour
         {
             for (int y = 0; y < max_y; y++)
             {
-                GameObject ti = (GameObject)Instantiate(tile, new Vector3(x+0.5f,y+0.5f, 1f), Quaternion.identity);
+                GameObject ti = (GameObject)Instantiate(tile, new Vector3(x + 0.5f, y + 0.5f, 1f), Quaternion.identity);
                 ti.transform.parent = tileParent.transform;
-                ti.name = "tile" + "_" + x.ToString()+"_"+ y.ToString();
+                ti.name = "tile" + "_" + x.ToString() + "_" + y.ToString();
                 target = "tile" + "_" + x.ToString() + "_" + y.ToString() + "/Canvas/RawImage";
                 image = GameObject.Find(target).GetComponent<RawImage>();
                 string path = imagePath + ((x + y) % 2).ToString();
@@ -174,7 +175,7 @@ public class GameManager : MonoBehaviour
             }
         }
     }
-    
+
 
     public void OnUpdate()
     {
@@ -209,24 +210,24 @@ public class GameManager : MonoBehaviour
             {
                 continue;
             }
-            for(int i = 0; i < pi.cells.Count; i++)
+            for (int i = 0; i < pi.cells.Count; i++)
             {
                 int roundX = (int)pi.cells[i].transform.position.x;
                 int roundY = (int)pi.cells[i].transform.position.y;
                 if (roundX < 0 || roundX >= max_x || roundY < 0 || roundY >= max_y) break;
-                for(int j = 0; j < 4; j++)
+                for (int j = 0; j < 4; j++)
                 {
-                    if (pi.cellScripts[i].wireInterfase[(j+pi.rotate)%4])
+                    if (pi.cellScripts[i].wireInterfase[(j + pi.rotate) % 4])
                     {
                         power_line[roundX * 2 + 1, roundY * 2 + 1, j] = 1;
-                        power_line[roundX * 2 + 1 + vx[j], roundY * 2 + 1 + vy[j], (j+2)%4] = 1;
+                        power_line[roundX * 2 + 1 + vx[j], roundY * 2 + 1 + vy[j], (j + 2) % 4] = 1;
 
                     }
                 }
             }
         }
 
-        distance[sx,sy] = 0;
+        distance[sx, sy] = 0;
         Queue<Tuple<int, int>> tq = new Queue<Tuple<int, int>>();
         tq.Enqueue(Tuple.Create(sx, sy));
         bool clear = true;
@@ -239,21 +240,21 @@ public class GameManager : MonoBehaviour
             {
                 int nx = x + vx[i];
                 int ny = y + vy[i];
-                if ((0 <= nx && nx <= max_x * 2) && (0 <= ny && ny <= max_y * 2) && power_line[x, y,i] == 1 && distance[nx, ny] == -1)
+                if ((0 <= nx && nx <= max_x * 2) && (0 <= ny && ny <= max_y * 2) && power_line[x, y, i] == 1 && distance[nx, ny] == -1)
                 {
                     GameObject bolt = (GameObject)Instantiate(lightning, new Vector3(0, 0, 0), Quaternion.identity);
                     bolt.transform.parent = lightningParent.transform;
                     DigitalRuby.LightningBolt.LightningBoltScript boltScript = bolt.GetComponent<DigitalRuby.LightningBolt.LightningBoltScript>();
-                    boltScript.StartPosition = new Vector3(x/2f, y/2f, -2f);
-                    boltScript.EndPosition = new Vector3(nx/2f, ny/2f, -2f);
+                    boltScript.StartPosition = new Vector3(x / 2f, y / 2f, -2f);
+                    boltScript.EndPosition = new Vector3(nx / 2f, ny / 2f, -2f);
                     distance[nx, ny] = distance[x, y] + 1;
                     tq.Enqueue(Tuple.Create(nx, ny));
                 }
             }
         }
-        for(int i = 0; i < elecposList.Count; i++)
+        for (int i = 0; i < elecposList.Count; i++)
         {
-            if (distance[elecposList[i].Item1*2+1, elecposList[i].Item2*2+1] == -1)
+            if (distance[elecposList[i].Item1 * 2 + 1, elecposList[i].Item2 * 2 + 1] == -1)
             {
                 clear = false;
             }
@@ -261,8 +262,18 @@ public class GameManager : MonoBehaviour
         if (distance[gx, gy] != -1 && clear)
         {
             Debug.Log("Clear");
-            SceneManager.LoadScene("ResultScene");
+            for (int i = 0; i < elecposList.Count; i++)
+            {
+                Instantiate(explosion, new Vector3(elecposList[i].Item1 + 0.5f, elecposList[i].Item2 + 0.5f, -20f), Quaternion.identity);
+            }
+            StartCoroutine(SceneChange());
         }
+    }
+
+    IEnumerator SceneChange()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("ResultScene");
     }
 
 
@@ -270,6 +281,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         data obj = jsonsettings.GetComponent<JsonSettings>().loadSettings();
         initializeMap(obj);
         initializePiece(obj);
